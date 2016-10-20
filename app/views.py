1b091d7ad955f475e6ccdf49356f7054a4d3607b
Josh Reed <jriddy@gmail.com>
2016-10-19 19:24:25 -0500

    just one route

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 import json
 
 from app import app
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    resp = {
+        'result': {
+            'description': 'Nothing happens yet',
+        },
+    }
+    return json.dumps(resp)
